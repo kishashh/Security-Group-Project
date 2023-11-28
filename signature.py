e27cc42031fd84825937f94c76ee11c6fe91afdb
Crisco8828 <crisco.dalton@yahoo.com>
2023-11-28 14:22:16 -0600

    openvc signature.py
    Commented out the initial code to replace it with code that utilizes "OpenVC" instead

--- signature.py
+++ signature.py
@@ -1,3 +1,5 @@
+''''
+
 from skimage import io, metrics
 import matplotlib.pyplot as plt
 
@@ -32,3 +34,42 @@ image_path2 = 'path/to/signature2.png'
 
 ssi_index = compare_signatures(image_path1, image_path2)
 print(f'Structural Similarity Index: {ssi_index:.2f}')
+
+'''
+
+import cv2
+import numpy as np
+
+def compare_signatures(template_path, target_path, threshold=0.8):
+    # Read the template and target images
+    template = cv2.imread(template_path, cv2.IMREAD_GRAYSCALE)
+    target = cv2.imread(target_path, cv2.IMREAD_GRAYSCALE)
+
+    # Perform template matching
+    result = cv2.matchTemplate(target, template, cv2.TM_CCOEFF_NORMED)
+    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
+
+    # If the maximum correlation coefficient is above the threshold, consider it a match
+    if max_val > threshold:
+        print("Signatures match!")
+        # You can also draw a rectangle around the matched region if needed
+        h, w = template.shape
+        top_left = max_loc
+        bottom_right = (top_left[0] + w, top_left[1] + h)
+        cv2.rectangle(target, top_left, bottom_right, 255, 2)
+        
+        # Display the images with the matching region highlighted
+        cv2.imshow('Matching Result', target)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+    else:
+        print("Signatures do not match.")
+
+# Example usage
+# template_path = 'path/to/signature_template.png'
+template_path = 'C:\Users\crisc\Downloads\signature (4).png'
+
+# target_path = 'path/to/target_image.png'
+target_path = 'C:\Users\crisc\Downloads\signature (5).png'
+
+compare_signatures(template_path, target_path)
