0900144deade3dc6106e84b6a967c222910b9e5e
Crisco8828 <crisco.dalton@yahoo.com>
2023-11-28 15:53:56 -0600

    Update to Comparing Signatures
    This tells the user if there is a potential match or not

--- signature.py
+++ signature.py
@@ -87,10 +87,59 @@ print(f'Structural Similarity Index: {ssi_index:.2f}')
 
 ####################################################################
 
+
+
+
+
+####################################################################
+
+# # Compare Signatures
+
+# import cv2
+# import numpy as np
+
+# def compare_signatures(template_path, target_path):
+#     # Read the template and target images
+#     template = cv2.imread(template_path, cv2.IMREAD_GRAYSCALE)
+#     target = cv2.imread(target_path, cv2.IMREAD_GRAYSCALE)
+
+#     # Initialize the ORB detector
+#     orb = cv2.ORB_create()
+
+#     # Find the key points and descriptors with ORB
+#     kp1, des1 = orb.detectAndCompute(template, None)
+#     kp2, des2 = orb.detectAndCompute(target, None)
+
+#     # Create a BFMatcher (Brute Force Matcher) object
+#     bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+
+#     # Match descriptors
+#     matches = bf.match(des1, des2)
+
+#     # Sort them in ascending order of distance
+#     matches = sorted(matches, key=lambda x: x.distance)
+
+#     # Draw the first 10 matches
+#     img_matches = cv2.drawMatches(template, kp1, target, kp2, matches[:10], None, flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+
+#     # Display the matches
+#     cv2.imshow('ORB Feature Matches', img_matches)
+#     cv2.waitKey(0)
+#     cv2.destroyAllWindows()
+
+# # Example usage
+# template_path = "C:\\Users\\crisc\\Downloads\\signature (4).png"
+# target_path = "C:\\Users\\crisc\\Downloads\\signature (5).png"
+
+# compare_signatures(template_path, target_path)
+
+
+##############################################################################################
+
 import cv2
 import numpy as np
 
-def compare_signatures(template_path, target_path):
+def compare_signatures(template_path, target_path, match_threshold=10):
     # Read the template and target images
     template = cv2.imread(template_path, cv2.IMREAD_GRAYSCALE)
     target = cv2.imread(target_path, cv2.IMREAD_GRAYSCALE)
@@ -111,6 +160,12 @@ def compare_signatures(template_path, target_path):
     # Sort them in ascending order of distance
     matches = sorted(matches, key=lambda x: x.distance)
 
+    # Check if the number of matches exceeds the threshold
+    if len(matches) >= match_threshold:
+        print(f"Signatures are a potential match! (Matches: {len(matches)})")
+    else:
+        print(f"Signatures do not match. (Matches: {len(matches)})")
+
     # Draw the first 10 matches
     img_matches = cv2.drawMatches(template, kp1, target, kp2, matches[:10], None, flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
 
@@ -119,8 +174,9 @@ def compare_signatures(template_path, target_path):
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
-# Example usage
+# Example usage with a match threshold of 10
 template_path = "C:\\Users\\crisc\\Downloads\\signature (4).png"
-target_path = "C:\\Users\\crisc\\Downloads\\signature (5).png"
+# target_path = "C:\\Users\\crisc\\Downloads\\signature (5).png"
+target_path = "C:\\Users\\crisc\\Downloads\\signature (6).png"
 
-compare_signatures(template_path, target_path)
+compare_signatures(template_path, target_path, match_threshold=10)
