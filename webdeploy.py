66bbef229346815fa4baa56b6c631dbbb3daaae0
CollierDalton <collierjdalton@gmail.com>
2023-12-04 01:30:39 -0600

    Added db connection to webdeploy
    Connected to the database in the webdeploy script. Also able to insert name and email into db from signup page.

--- webdeploy.py
+++ webdeploy.py
@@ -1,10 +1,41 @@
-import cv2, numpy, os,time
+import cv2, numpy, os, time, pyodbc
 from flask import Flask, render_template, request, redirect, url_for, Response
+
+# Set your connection details
+server = 'computer-security-fortnite-group.database.windows.net'
+database = 'is-this-you-db'
+username = 'fortnite'
+password = '5Nights!'
+driver = '{ODBC Driver 17 for SQL Server}'  # Use the correct driver version
+
+# Build the connection string
+connection_string = f'DRIVER={driver};SERVER={server};DATABASE={database};UID={username};PWD={password}'
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
 @app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('signup.html')
+
+@app.route('/signup_form', methods=['POST'])
+def signup_form():
+    firstname = request.form['fname']
+    lastname = request.form['lname']
+    email = request.form['email']
+
+    try:
+        # Connect to the database
+        connection = pyodbc.connect(connection_string)
+        cursor = connection.cursor()
+
+        cursor.execute('INSERT INTO Users (firstname, lastname, email) VALUES (?, ?, ?)', (firstname, lastname, email))
+        connection.commit()
+    except Exception as e:
+        print(f"Error: {e}")
+    finally:
+        if 'connection' in locals():
+            connection.close()
+    
+    return 'Name and email insterted succesfully'
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
