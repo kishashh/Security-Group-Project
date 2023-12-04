250b0c67d6ff38c2a9deea5802252f63b94c604a
Crisco8828 <crisco.dalton@yahoo.com>
2023-12-03 19:21:30 -0600

    No errors here on app.py
    Added an if statement so that comparing signatures doesn't just spit out an error now

--- app.py
+++ app.py
@@ -1,16 +1,28 @@
-# Integrating my python code into my html code (index.html)
-
 from flask import Flask, render_template, request
 import cv2
 import numpy as np
-import os
+import base64
 
 app = Flask(__name__)
 
-def compare_signatures(template_path, target_path, match_threshold=130):
-    # Your existing compare_signatures function here
+def compare_signatures(template_path, target_data, match_threshold=130):
 
-    # Read the template and target images
+    target_data_parts = target_data.split(',')
+    
+    if len(target_data_parts) < 2:
+        print("Invalid target data format.")
+        return  # or handle the error in another way
+    
+    target_bytes = base64.b64decode(target_data_parts[1])
+    target_np = np.frombuffer(target_bytes, np.uint8)
+    target = cv2.imdecode(target_np, cv2.IMREAD_GRAYSCALE)
+
+    # # Convert base64 image data to NumPy array
+    # target_bytes = base64.b64decode(target_data.split(',')[1])
+    # target_np = np.frombuffer(target_bytes, np.uint8)
+    # target = cv2.imdecode(target_np, cv2.IMREAD_GRAYSCALE)
+
+    # ... (rest of the code remains unchanged)
     template = cv2.imread(template_path, cv2.IMREAD_GRAYSCALE)
     target = cv2.imread(target_path, cv2.IMREAD_GRAYSCALE)
 
@@ -44,35 +56,36 @@ def compare_signatures(template_path, target_path, match_threshold=130):
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
+    # Check if the number of matches exceeds the threshold
+    if len(matches) >= match_threshold:
+        return f"Signatures are a potential match! (Matches: {len(matches)})"
+    else:
+        return f"Signatures do not match. (Matches: {len(matches)})"
+    
+    # If the number of matches is less than 130, display a message
+    if len(matches) < 130:
+        print("Insufficient matches for meaningful comparison. Increase match_threshold or check the input images.")
+    else:
+        # Draw the first 10 matches
+        img_matches = cv2.drawMatches(template, kp1, target, kp2, matches[:130], None, flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+
+        # Display the matches
+        cv2.imshow('ORB Feature Matches', img_matches)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/upload', methods=['POST'])
-def upload():
-    if 'file' not in request.files:
-        return render_template('index.html', error='No file part')
+@app.route('/compare', methods=['POST'])
+def compare():
+    # template_path = "path/to/your/template/image.png"  # Set the path to your template image
+    template_path = "C:\\Users\\crisc\\Downloads\\signature (4).png" # Test Signature
 
-    file = request.files['file']
-
-    if file.filename == '':
-        return render_template('index.html', error='No selected file')
-
-    # Save the uploaded file to a temporary location
-    temp_path = 'temp_uploaded_image.png'
-    file.save(temp_path)
-
-    # Define the paths for the template and target images
-    template_path = "C:\\Users\\crisc\\Downloads\\signature (4).png"
-    target_path = temp_path
-
-    # Call the compare_signatures function
-    compare_signatures(template_path, target_path, match_threshold=130)
-
-    # Remove the temporary uploaded file
-    os.remove(temp_path)
-
-    return render_template('index.html', success='Comparison completed successfully')
+    target_data = request.form['signatureData']
+    result = compare_signatures(template_path, target_data, match_threshold=130)
+    return render_template('index.html', result=result)
 
 if __name__ == '__main__':
     app.run(debug=True)
